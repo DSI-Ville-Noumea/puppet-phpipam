63a3665cb4f41553480e13af60e7a3134e3b2971
Baptiste <baptiste.jammet@ville-noumea.nc>
2017-06-23 15:30:29 +1100

    test

--- manifests/config.pp
+++ manifests/config.pp
@@ -149,9 +149,9 @@ class phpipam::config {
     user    => 'root',
   }
 
-  firewall { 'Open_80':
+  firewall { 'Open80':
     proto  => 'tcp',
-    port   => 80,
+    port   => '80',
     action => 'accept',
   }
 }
